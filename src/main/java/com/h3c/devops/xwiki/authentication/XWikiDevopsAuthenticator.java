2a999976fae0855b1d3cca29d78b67729b0caa46
origin <15761635397@163.com>
2019-01-18 18:13:07 +0800

    Initial commit

---
+++ src/main/java/com/h3c/devops/xwiki/authentication/XWikiDevopsAuthenticator.java
@@ -0,0 +1,299 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.h3c.devops.xwiki.authentication;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import com.h3c.devops.xwiki.authentication.internal.UserUtils;
+import com.h3c.devops.xwiki.authentication.internal.XWikiDevopsNgConfig;
+import net.sf.json.JSONObject;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.securityfilter.filter.SecurityRequestWrapper;
+import org.securityfilter.realm.SimplePrincipal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.user.api.XWikiUser;
+import com.xpn.xwiki.user.impl.xwiki.XWikiAuthServiceImpl;
+import com.xpn.xwiki.web.XWikiRequest;
+
+/**
+ * Authentication based on Jasig CAS server. It creates XWiki users if they have never logged in before and synchronizes
+ * membership to XWiki groups based on membership to CAS group field mapping.
+ * <p>
+ * Some parameters can be used to customized its behavior in xwiki.cfg:
+ * <ul>
+ * <li>xwiki.authentication.cas.server: CAS server url (i.e. https://localhost:8443/cas)</li>
+ * <li>xwiki.authentication.cas.protocol: used protocol CAS20 or SAML11</li>
+ * <li>xwiki.authentication.cas.access_denied_page: user not authorized page (i.e.
+ * /bin/view/XWiki/XWikiCASAccessDenied). If not set a HTTP status 401 is returned.</li>
+ * <li>xwiki.authentication.cas.create_user: 0 or 1 if create XWiki user after log in</li>
+ * <li>xwiki.authentication.cas.update_user: 0 or 1 if update user attributes after every log in</li>
+ * <li>xwiki.authentication.cas.fields_mapping: mapping between XWiki user profile values and CAS attributes. Example
+ * (xwiki-attribute=cas-attribute,...): <code>last_name=lastName,first_name=firstName,email=email</code></li>
+ * <li>xwiki.authentication.cas.group_field: CAS attribute name which contains group membership</li>
+ * <li>xwiki.authentication.cas.group_mapping: Maps XWiki groups to CAS groups, separator is "|".
+ * XWiki.XWikiAdminGroup=cn=AdminRole,ou=groups,o=domain,c=com| XWiki.CASUsers=ou=groups,o=domain,c=com</li>
+ * </ul>
+ *
+ * @version $Id$
+ */
+public class XWikiDevopsAuthenticator extends XWikiAuthServiceImpl
+{
+
+    /** LogFactory <code>LOGGER</code>. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(XWikiDevopsAuthenticator.class);
+
+    /**
+     * The XWiki space where users are stored.
+     */
+    private static final String XWIKI_USER_SPACE = "XWiki";
+
+    /**
+     * Request wrapper auth method
+     */
+    private static final String AUTH_METHOD = "DEVOPS";
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see com.xpn.xwiki.user.impl.xwiki.AppServerTrustedAuthServiceImpl#checkAuth(com.xpn.xwiki.XWikiContext)
+     */
+    @Override
+    public XWikiUser checkAuth(XWikiContext context) throws XWikiException
+    {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("devops authentication started");
+        }
+
+        SecurityRequestWrapper wrappedRequest =
+                new SecurityRequestWrapper(context.getRequest().getHttpServletRequest(), null, null, AUTH_METHOD);
+
+        if ("login".equals(context.getAction())) {
+            return doLogin(wrappedRequest,context);
+        } else if ("logout".equals(context.getAction()) && wrappedRequest.getUserPrincipal() != null) {
+            // TODO redirect to the devops logout page
+
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("User " + wrappedRequest.getUserPrincipal().getName() + " has been logged-out");
+            }
+            wrappedRequest.setUserPrincipal(null);
+
+            // XWikiDevopsNgConfig config = XWikiDevopsNgConfig.getInstance();
+            //
+            // String casServer = config.getDEVOPSParam("cas_server", "", context);
+            //
+            // try {
+            // context.getResponse().sendRedirect(context.getResponse().encodeRedirectURL(casServer
+            // + "/logout"));
+            // } catch (IOException e) {
+            // throw new XWikiException(XWikiException.MODULE_XWIKI_USER,
+            // XWikiException.ERROR_XWIKI_USER_INIT,
+            // "Can't redirect to the CAS logout page", e);
+            // }
+            return null;
+
+        }
+
+        if (wrappedRequest.getUserPrincipal() == null) {
+            return null;
+        }
+        return new XWikiUser(wrappedRequest.getUserPrincipal().getName());
+    }
+    private XWikiUser doLogin(SecurityRequestWrapper wrappedRequest,XWikiContext context) throws XWikiException {
+        String token = getTokenFromCookie(context.getRequest());
+        XWikiDevopsNgConfig config = XWikiDevopsNgConfig.getInstance();
+        if (token == null) {
+            // redirect to the devops NG login page
+            try {
+
+                String devopsServer = config.getDEVOPSParam("devops_server", "", context);
+
+                String serviceUrl = URLEncoder.encode(createServiceUrl(context), "UTF-8");
+                LOGGER.info("callback service url {}",serviceUrl);
+                context.getResponse().sendRedirect(
+                        context.getResponse().encodeRedirectURL(devopsServer + "/login?callback=" + serviceUrl));
+            } catch (IOException e) {
+                throw new XWikiException(XWikiException.MODULE_XWIKI_USER, XWikiException.ERROR_XWIKI_USER_INIT,
+                        "Can't redirect to the CAS login page", e);
+            }
+            return null;
+
+        } else {
+            // authenticate using devops token
+
+            Principal principal = authenticate(token, context);
+
+            if (principal != null) {
+                // login successful
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("User " + principal.getName() + " has been logged-in");
+                }
+
+                // invalidate old session if the user was already
+                // authenticated,
+                // and they logged in as a different user
+                if (wrappedRequest.getUserPrincipal() != null
+                        && !principal.getName().equals(wrappedRequest.getRemoteUser())) {
+                    wrappedRequest.getSession().invalidate();
+                }
+            } else {
+                String failedPage = config.getDEVOPSParam("cas_access_denied_page", null, context);
+                try {
+                    if (failedPage != null) {
+                        context.getResponse().sendRedirect(
+                                context.getResponse().encodeRedirectURL(
+                                        context.getRequest().getContextPath() + failedPage));
+                    } else {
+                        context.getResponse().sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                    }
+                } catch (IOException e) {
+                    throw new XWikiException(XWikiException.MODULE_XWIKI_USER,
+                            XWikiException.ERROR_XWIKI_USER_INIT, "Can't authenticate user", e);
+
+                }
+            }
+            wrappedRequest.setUserPrincipal(principal);
+        }
+        return null;
+    }
+    private String getTokenFromCookie(XWikiRequest request){
+        Cookie tokenCookie = request.getCookie("token");
+        if (null == tokenCookie || tokenCookie.getValue() == null || "".equals(tokenCookie.getValue())){
+            LOGGER.error("get token fail");
+            return null;
+        }
+        return tokenCookie.getValue();
+    }
+    /**
+     * Validate CAS ticket. If success return a principal
+     *
+     * @param token CAS ticket to validate
+     * @param context
+     * @return principal of the authenticated user
+     * @throws XWikiException
+     */
+    public Principal authenticate(String token, XWikiContext context) throws XWikiException
+    {
+        Principal principal = null;
+
+        XWikiDevopsNgConfig config = XWikiDevopsNgConfig.getInstance();
+
+        String devopsServer = config.getDEVOPSParam("devops_server", "", context);
+        String devopsLoginUri = config.getDEVOPSParam("devops_login_uri", "", context);
+        String devopsAuthInfoUrl = config.getDEVOPSParam("devops_info_uri","",context);
+        LOGGER.info("devopsServer {}",devopsServer);
+        LOGGER.info("devopsLoginUri {}",devopsLoginUri);
+        LOGGER.info("devopsAuthInfoUrl",devopsAuthInfoUrl);
+
+            // using token cookie to get auth info from devops
+            // service url creation
+            String serviceUrl = createServiceUrl(context);
+            LOGGER.info("service url {}", serviceUrl);
+            // get user info by cookie token
+            String userInfoUrl = "";
+            JSONObject devopsUserInfo = getDevopsUserInfo(userInfoUrl, token);
+            if (devopsUserInfo == null) {
+                // todo error login need redirect to devops login
+            }
+
+            // get valid wiki username
+            String devopsFieldMappings = config.getDEVOPSParam("devops_field_mappings", "", context);
+            String validXWikiUserName = "testuser1";
+            String database = context.getDatabase();
+            // Switch to main wiki to force users to be global users
+            context.setDatabase(context.getMainXWiki());
+
+            // user profile
+            XWikiDocument userProfile =
+                    context.getWiki().getDocument(
+                            new DocumentReference(context.getDatabase(), XWIKI_USER_SPACE, validXWikiUserName), context);
+
+            boolean isNewUser = userProfile.isNew();
+
+            // create XWiki principal
+            principal = new SimplePrincipal(context.getDatabase() + ":" + userProfile.getFullName());
+
+            if (!config.getDEVOPSParam("cas_create_user", "0", context).equals("1")) {
+                // user creation is disabled
+                if (isNewUser) {
+                    return null;
+                }
+
+                return principal;
+            }
+
+            // update or create user
+            UserUtils.syncUser(userProfile, null, context);
+
+        return principal;
+    }
+
+    /**
+     * Create a devops service url
+     *
+     * @param context
+     * @return
+     */
+    private String createServiceUrl(XWikiContext context)
+    {
+        XWikiRequest request = context.getRequest();
+        StringBuilder sb = new StringBuilder();
+        String wikiHome = context.getWiki().Param("xwiki.home");
+        if (wikiHome != null) {
+            sb.insert(0, request.getRequestURI());
+            sb.deleteCharAt(0);
+            sb.insert(0, wikiHome);
+        } else {
+            sb.insert(0, request.getRequestURL());
+        }
+        return sb.toString();
+    }
+
+
+    private JSONObject getDevopsUserInfo(String userInfoUrl,String token){
+        try {
+            HttpClient httpClient = new HttpClient();
+            GetMethod getMethod = new GetMethod(userInfoUrl);
+            getMethod.addRequestHeader("Cookie","token=" + token);
+            httpClient.executeMethod(getMethod);
+            String info = getMethod.getResponseBodyAsString();
+            LOGGER.info("devops info {}",info);
+            return JSONObject.fromObject(info);
+        } catch (IOException e) {
+            LOGGER.error("get devops user info fail");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/h3c/devops/xwiki/authentication/internal/XWikiDevopsNgConfig.java
@@ -0,0 +1,290 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.h3c.devops.xwiki.authentication.internal;
+
+import com.xpn.xwiki.XWikiContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Access to CAS configurations.
+ * 
+ * @version $Id$
+ */
+public final class XWikiDevopsNgConfig
+{
+
+    /**
+     * Mapping fields separator.
+     */
+    public static final String DEFAULT_SEPARATOR = ",";
+
+    /**
+     * CAS properties names prefix in xwiki.cfg.
+     */
+    public static final String XWIKI_AUTHENTICATION_DEVOPS = "xwiki.authentication.devops.";
+
+    /**
+     * CAS properties names prefix in XWikiPreferences.
+     */
+    public static final String PREF_DEVOPS_PREFIX = "devops_";
+
+    /**
+     * Mapping fields separator.
+     */
+    public static final String USERMAPPING_SEP = DEFAULT_SEPARATOR;
+
+    /**
+     * Character user to link XWiki field name and CAS field name in user mappings property.
+     */
+    public static final String USERMAPPING_XWIKI_DEVOPS_LINK = "=";
+
+    public static final String PROTOCOL_CAS20 = "CAS20";
+
+    public static final String PROTOCOL_SAML11 = "SAML11";
+
+    /**
+     * Logging tool.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(XWikiDevopsNgConfig.class);
+
+    /**
+     * Unique instance of {@link XWikiDevopsNgConfig}.
+     */
+    private static XWikiDevopsNgConfig instance;
+
+    /**
+     * Protected constructor. Use {@link #getInstance()}.
+     */
+    private XWikiDevopsNgConfig()
+    {
+    }
+
+    /**
+     * @return unique instance of {@link XWikiDevopsNgConfig}.
+     */
+    public static XWikiDevopsNgConfig getInstance()
+    {
+        if (instance == null) {
+            instance = new XWikiDevopsNgConfig();
+        }
+
+        return instance;
+    }
+
+    /**
+     * First try to retrieve value from XWiki Preferences and then from xwiki.cfg Syntax cas_*name* (for XWiki
+     * Preferences) will be changed to cas.*name* for xwiki.cfg.
+     * 
+     * @param prefName the name of the property in XWikiPreferences.
+     * @param cfgName the name of the property in xwiki.cfg.
+     * @param def default value.
+     * @param context the XWiki context.
+     * @return the value of the property.
+     */
+    public String getDEVOPSParam(String prefName, String cfgName, String def, XWikiContext context)
+    {
+        String param = null;
+
+        try {
+            param = context.getWiki().getXWikiPreference(prefName, context);
+        } catch (Exception e) {
+            LOGGER.error("Failed to get preferences", e);
+        }
+
+        if (param == null || "".equals(param)) {
+            try {
+                param = context.getWiki().Param(cfgName);
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+
+        if (param == null) {
+            param = def;
+        }
+
+        return param;
+    }
+
+    /**
+     * First try to retrieve value from XWiki Preferences and then from xwiki.cfg Syntax cas_*name* (for XWiki
+     * Preferences) will be changed to cas.*name* for xwiki.cfg.
+     * 
+     * @param name the name of the property in XWikiPreferences.
+     * @param def default value.
+     * @param context the XWiki context.
+     * @return the value of the property.
+     */
+    public String getDEVOPSParam(String name, String def, XWikiContext context)
+    {
+        return getDEVOPSParam(name, name.replace(PREF_DEVOPS_PREFIX, XWIKI_AUTHENTICATION_DEVOPS), def, context);
+    }
+
+    /**
+     * First try to retrieve value from XWiki Preferences and then from xwiki.cfg Syntax cas_*name* (for XWiki
+     * Preferences) will be changed to cas.*name* for xwiki.cfg.
+     * 
+     * @param name the name of the property in XWikiPreferences.
+     * @param def default value.
+     * @param context the XWiki context.
+     * @return the value of the property.
+     */
+    public long getCASParamAsLong(String name, long def, XWikiContext context)
+    {
+        String paramStr =
+            getDEVOPSParam(name, name.replace(PREF_DEVOPS_PREFIX, XWIKI_AUTHENTICATION_DEVOPS), String.valueOf(def), context);
+
+        long value;
+
+        try {
+            value = Long.valueOf(paramStr);
+        } catch (Exception e) {
+            value = def;
+        }
+
+        return value;
+    }
+
+    public String getCASProtocol(XWikiContext context)
+    {
+        return getDEVOPSParam("cas_protocol", PROTOCOL_CAS20, context);
+    }
+
+    public boolean isCAS20Protocol(XWikiContext context)
+    {
+        return PROTOCOL_CAS20.equalsIgnoreCase(getCASProtocol(context));
+    }
+
+    public boolean isSAML11Protocol(XWikiContext context)
+    {
+        return PROTOCOL_SAML11.equalsIgnoreCase(getCASProtocol(context));
+    }
+
+    /**
+     * Get mapping between XWiki groups names and CAS groups names.
+     * 
+     * @param context the XWiki context.
+     * @return the mapping between XWiki users and CAS users. The key is the XWiki group, and the value is the list of
+     *         mapped LDAP groups.
+     */
+    public Map<String, Set<String>> getGroupMappings(XWikiContext context)
+    {
+        Map<String, Set<String>> groupMappings = new HashMap<String, Set<String>>();
+
+        String param = getDEVOPSParam("cas_group_mapping", "", context);
+
+        if (param.trim().length() > 0) {
+            char[] buffer = param.trim().toCharArray();
+            boolean escaped = false;
+            StringBuilder mapping = new StringBuilder(param.length());
+            for (int i = 0; i < buffer.length; ++i) {
+                char c = buffer[i];
+
+                if (escaped) {
+                    mapping.append(c);
+                    escaped = false;
+                } else {
+                    if (c == '\\') {
+                        escaped = true;
+                    } else if (c == '|') {
+                        addGroupMapping(mapping.toString(), groupMappings);
+                        mapping.setLength(0);
+                    } else {
+                        mapping.append(c);
+                    }
+                }
+            }
+
+            if (mapping.length() > 0) {
+                addGroupMapping(mapping.toString(), groupMappings);
+            }
+        }
+
+        return groupMappings;
+    }
+
+    /**
+     * @param mapping the mapping to parse
+     * @param groupMappings the map to add parsed group mapping to
+     */
+    private void addGroupMapping(String mapping, Map<String, Set<String>> groupMappings)
+    {
+        int splitIndex = mapping.indexOf('=');
+
+        if (splitIndex < 1) {
+            LOGGER.error("Error parsing cas_group_mapping attribute [{}]", mapping);
+        } else {
+            String xwikigroup = mapping.substring(0, splitIndex);
+            String casgroup = mapping.substring(splitIndex + 1);
+
+            Set<String> casGroups = groupMappings.get(xwikigroup);
+
+            if (casGroups == null) {
+                casGroups = new HashSet<String>();
+                groupMappings.put(xwikigroup, casGroups);
+            }
+
+            casGroups.add(casgroup);
+
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Groupmapping found [{}] [{}]", xwikigroup, casGroups);
+            }
+        }
+    }
+
+    /**
+     * Get mapping between XWiki users attributes and CAS users attributes.
+     * 
+     * @param context the XWiki context.
+     * @return the mapping between XWiki groups and CAS groups.
+     */
+    public Map<String, String> getUserMappings(XWikiContext context)
+    {
+        Map<String, String> userMappings = new HashMap<String, String>();
+
+        String casFieldMapping = getDEVOPSParam("cas_fields_mapping", null, context);
+
+        if (casFieldMapping != null && casFieldMapping.length() > 0) {
+            String[] fields = casFieldMapping.split(USERMAPPING_SEP);
+
+            for (int j = 0; j < fields.length; j++) {
+                String[] field = fields[j].split(USERMAPPING_XWIKI_DEVOPS_LINK);
+                if (2 == field.length) {
+                    String xwikiattr = field[0].replace(" ", "");
+                    String casattr = field[1].replace(" ", "");
+
+                    userMappings.put(casattr, xwikiattr);
+
+                } else {
+                    LOGGER.error("Error parsing cas_fields_mapping attribute in xwiki.cfg: " + fields[j]);
+                }
+            }
+        }
+
+        return userMappings;
+    }
+
+}
